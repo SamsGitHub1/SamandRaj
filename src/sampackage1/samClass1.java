93b642b659fab00393013b0fad9983ef9bf89918
SamsGitHub1 <44011188+SamsGitHub1@users.noreply.github.com>
2020-02-08 19:31:00 +0530

    First commit by Sam

---
+++ src/sampackage1/samClass1.java
@@ -0,0 +1,9 @@
+package sampackage1;
+
+public class samClass1 
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is samclass1 - line written by sam");
+	}
+}
---
+++ src/sampackage1/samClass2.java
@@ -0,0 +1,9 @@
+package sampackage1;
+
+public class samClass2 
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is samclass2 - line written by sam");
+	}
+}
