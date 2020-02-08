e5a9f7b6d041e025bde41608da340bd65b661c9b
SamsGitHub1 <44011188+SamsGitHub1@users.noreply.github.com>
2020-02-08 21:18:51 +0530

    merger

--- src/sampackage1/samClass1.java
+++ src/sampackage1/samClass1.java
@@ -6,6 +6,7 @@ public class samClass1
 	{
 		System.out.println("This is samclass1 - line written by sam raj kiran");
 		System.out.println("This is samclass1 - line2 written by raj");
+		System.out.println("This is samclass1 - line3 written by sam");
 
 	}
 }
