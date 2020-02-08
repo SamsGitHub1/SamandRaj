01cd6c6a567ea48f1604368ac149571511f4fa0a
ravituduru <tudururavikiran@gmail.com>
2020-02-08 20:51:11 +0530

    Rajkiran added line2

--- src/sampackage1/samClass1.java
+++ src/sampackage1/samClass1.java
@@ -5,5 +5,7 @@ public class samClass1
 	public static void main(String args[])
 	{
 		System.out.println("This is samclass1 - line written by sam");
+		System.out.println("This is samclass1 - line2 written by raj");
+
 	}
 }
