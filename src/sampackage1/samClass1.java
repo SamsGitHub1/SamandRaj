a193449157229dd8840ec970e06138354d8a51fc
ravituduru <tudururavikiran@gmail.com>
2020-02-08 20:56:43 +0530

    changed sam line

--- src/sampackage1/samClass1.java
+++ src/sampackage1/samClass1.java
@@ -4,7 +4,7 @@ public class samClass1
 {
 	public static void main(String args[])
 	{
-		System.out.println("This is samclass1 - line written by sam");
+		System.out.println("This is samclass1 - line written by sam raj kiran");
 		System.out.println("This is samclass1 - line2 written by raj");
 
 	}
